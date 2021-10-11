50097433b80a4d3df0e515453a6a51ce4b77fc32
shubham <shubhdgaikwad@gmail.com>
2021-10-11 20:12:05 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mqtt-web';
+
+  constructor(private http: HttpClient) { }
+
+  private apiServerUrl = environment.apiBaseUrl;
+
+  publishMessage(event: String) {
+
+    console.log("url=>" + this.apiServerUrl + "/topic/" + event);
+
+    this.http.get(`${this.apiServerUrl}/topic/${event}`).subscribe(
+      (response: any) => {
+        console.log(response);
+      }, (error: HttpErrorResponse) => {
+        console.log(error);
+      });
+
+  }
 }
